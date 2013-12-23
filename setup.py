1405f410b5b4928b0e098f42ed0178b85c21f18a
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2013-12-23 12:41:28 -0800

    begin v0.3 development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='egg',
-    version='0.1',
+    version='0.3',
     py_modules=['egg'],
     entry_points=dict(console_scripts=['egg = egg:main']),
 )
