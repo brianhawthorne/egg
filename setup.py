d2961eff387acecda387dc86f56802fb6c39896f
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2016-02-24 17:47:45 -0800

    bump version to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='egg',
-    version='0.3',
+    version='0.4',
     py_modules=['egg'],
     entry_points=dict(console_scripts=['egg = egg:main']),
 )
